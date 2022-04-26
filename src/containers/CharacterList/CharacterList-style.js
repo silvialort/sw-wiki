1c3a94c33453e831649c185159f2d74ac753a9bb
silvialort <silvialort@gmail.com>
2022-04-26 11:55:52 +0200

    Adding style to Character List

--- src/containers/CharacterList/CharacterList-style.js
+++ src/containers/CharacterList/CharacterList-style.js
@@ -1,6 +1,12 @@
-import styled from styled-components;
+import styled from "styled-components";
 
-const CharacterListContainer = styled.ul``;
+const CharacterListContainer = styled.ul`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    align-items: flex-start;
+    flex-flow: row wrap
+    `;
 
 
 export default CharacterListContainer;
