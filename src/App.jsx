2edda19006e4fe75bd671fcfaeb71bc49b2f0888
silvialort <silvialort@gmail.com>
2022-04-26 12:13:22 +0200

    Adding fonts to the proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Header from './containers/Header/Header';
+import CharacterList from './containers/CharacterList/CharacterList';
+import Card from './components/Card/Card';
+
 function App() {
   return (
     <>
+      <Header></Header>
       <h1>Prueba</h1>
+      <CharacterList>
+          <Card name="Luke Skywalker" />
+      </CharacterList>
     </>
   );
 }
