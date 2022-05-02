a5488aa29015996586aaa61aac4d3d4e4b28ac20
silvialort <silvialort@gmail.com>
2022-05-02 16:28:25 +0200

    Wiki creation

--- src/pages/Personajes/index.jsx
+++ src/pages/Personajes/index.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import { useCharacters } from "../../services/sw-services";
+
+import Header from '../../containers/Header/Header';
+import CharacterList from '../../containers/CharacterList/CharacterList';
+import Section from '../../containers/Section/Section';
+
+
+import Card from '../../components/Card/Card';
 
 function Personajes () {
+
+    const characterService = useCharacters();
+
+    const [characterList, setCharacterList] = useState([]);
+
+    useEffect(() => {
+        const getCharacterList = async () => {
+            const characters = await characterService.getCharacters();
+            const { results } = await characters.data;
+            setCharacterList(results);
+        }
+        getCharacterList();
+    }, []);
+
     return(
         <>
-            <h1>Esto es la página de personajes</h1>
+            <Header />
+            <Section>
+                <CharacterList>
+                    {
+                        characterList.map((character, index) => {
+                            return(
+                                <li key={index}>
+                                    <Card
+                                    name={character.name}
+
+                                    />
+                                </li>
+                            )
+                    })}
+                </CharacterList>
+            </Section>
+
         </>
     )
 
