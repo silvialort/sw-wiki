49616548c0531b0d1b77b64b9eb50d1ee6c9320c
silvialort <silvialort@gmail.com>
2022-05-02 17:28:13 +0200

    Wiki creation

--- src/pages/Personajes/index.jsx
+++ src/pages/Personajes/index.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
 
+// API
 import { useCharacters } from "../../services/sw-services";
 
+// Componentes y contenedores
 import Header from '../../containers/Header/Header';
+import Main from '../../containers/Main/Main';
 import CharacterList from '../../containers/CharacterList/CharacterList';
 import Section from '../../containers/Section/Section';
-
-
 import Card from '../../components/Card/Card';
+import Input from '../../components/Input/Input';
+import Select from '../../components/Select/Select';
 
 function Personajes () {
 
     const characterService = useCharacters();
-
     const [characterList, setCharacterList] = useState([]);
 
     useEffect(() => {
@@ -27,21 +29,26 @@ function Personajes () {
     return(
         <>
             <Header />
-            <Section>
-                <CharacterList>
-                    {
-                        characterList.map((character, index) => {
-                            return(
-                                <li key={index}>
-                                    <Card
-                                    name={character.name}
+            <Main>
+                <Section>
+                    <Input text='Buscar'/>
+                    <Select></Select>
+                    <Select></Select>
+                    <CharacterList>
+                        {
+                            characterList.map((character, index) => {
+                                return(
+                                    <li key={index}>
+                                        <Card
+                                        name={character.name}
 
-                                    />
-                                </li>
-                            )
-                    })}
-                </CharacterList>
-            </Section>
+                                        />
+                                    </li>
+                                )
+                        })}
+                    </CharacterList>
+                </Section>
+            </Main>
 
         </>
     )
