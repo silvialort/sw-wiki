85bc26545f5b42c677b6279a549546311b0ba298
unknown <silvialort>
2022-05-03 11:51:06 +0200

    Create home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 import Header from '../../containers/Header/Header';
+import Main from "../../containers/Main/Main";
+import Section from "../../containers/Section/Section";
+import Heading1 from '../../components/Texts/Headings/H1/Heading1';
+import Heading2 from '../../components/Texts/Headings/H2/Heading2';
+import Button from "../../components/Button/Button";
+
+// import background from '../assets/background.webp';
 
 function Home () {
 
     return(
         <>
             <Header />
-            <h1>Esto es la home</h1>
+            <Main>
+                <Section
+                bg="black">
+                    <Heading1
+                    text='Star Wars Wiki' 
+                    color='#FFFF'
+                    />
+                </Section>
+            </Main>
         </>
     )
 }
