c5ac2e93b66517e301dabaf994e7aeccecd0df10
unknown <silvialort>
2022-05-03 11:53:14 +0200

    Changes in styled components

--- src/containers/Section/Section-style.js
+++ src/containers/Section/Section-style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const SectionContainer = styled.section`
     width: 100%;
     min-height: 50vh;
-    background-color: ${(props) => props.bg || '#F3F7FF'};
+    background-color: ${(props) => props.bgcl || '#F3F7FF'};
     display: flex;
     padding: 20px;
     flex-flow: ${(props) => props.direction || 'row'} wrap;
