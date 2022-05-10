f68d685faa9963113afc4ff15446003a0fdea46b
silvialort <silvialort@gmail.com>
2022-05-10 12:08:35 +0200

    Container component created

---
+++ src/containers/Container/Container-style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ContainerDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+    `;
+
+
+export default ContainerDiv;
