575a1283a5441eb2d057eab5cebac596e47814b1
silvialort <silvialort@gmail.com>
2022-05-16 18:04:48 +0200

    Added pagination

--- src/pages/Planetas/index.jsx
+++ src/pages/Planetas/index.jsx
@@ -20,14 +20,42 @@ function Planetas () {
     const { handleModal, modalOpened } = useModal();
     const planetService = usePlanets();
 
+    let [currentPage, setCurrentPage] = useState(1);
+
     const [planetList, setPlanetList] = useState([]);
     const [filteredplanetList, setFilteredPlanetList] = useState([]);
     const [selectedPlanet, setSelectedPlanet] = useState({});
 
+    const nextPage = () => {
+        if (currentPage <= 6) {
+            currentPage++;
+            setCurrentPage(currentPage);
+            handlePage();
+            console.log(currentPage);
+        }
+
+    }
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            currentPage--;
+            setCurrentPage(currentPage);
+            console.log(currentPage);
+            handlePage();
+        }
+    }
+
+    const handlePage = async () => {
+        const planets = await planetService.getPlanets(currentPage);
+        const { results } = await planets.data;
+        setPlanetList(results);
+        setFilteredPlanetList(results);
+    }
+
     useEffect(() => {
 
         const getPlanetsList = async () => {
-            const planets = await planetService.getPlanets();
+            const planets = await planetService.getPlanets(currentPage);
             const { results } = await planets.data;
             setPlanetList(results);
             setFilteredPlanetList(results);
@@ -71,16 +99,27 @@ function Planetas () {
 
                 <MainModal handleClick={() => handleModal(false)}>
                     <ModalContent >
+                        <h2>{selectedPlanet.name}</h2>
                         <ul>
-                        <li>{selectedPlanet.diameter}</li>
+                        <li>Periodo de rotación: {selectedPlanet.rotation_period}</li>
+                        <li>Diámetro: {selectedPlanet.diameter}</li>
+                        <li>Periodo orbital: {selectedPlanet.orbital_period}</li>
+                        <li>Clima: {selectedPlanet.climate}</li>
+                        <li>Gravedad: {selectedPlanet.gravity}</li>
+                        <li>Terreno: {selectedPlanet.terrain}</li>
+                        <li>Población: {selectedPlanet.population}</li>
                         </ul>
                     </ModalContent>
                 </MainModal>
                 )
             }
             <Container>
-                <Button text='Anterior' />
-                <Button text='Siguiente' />
+                <Button
+                text='Anterior'
+                handleClick={() => prevPage()} />
+                <Button
+                text='Siguiente'
+                handleClick={() => nextPage()} />
             </Container>
             </Section>
             </Main>
