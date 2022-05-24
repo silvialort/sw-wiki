ebfd1adc008604247eabd1188f84cddd45b09a0a
silvialort <silvialort@gmail.com>
2022-05-24 12:31:09 +0200

    Created form to Contacto page

--- src/pages/Contacto/index.jsx
+++ src/pages/Contacto/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Header from '../../containers/Header/Header';
 import Footer from '../../containers/Footer/Footer';
@@ -11,23 +11,45 @@ import HeadingH1 from '../../components/Texts/Headings/H1/Heading1';
 
 function Contact () {
 
+    const [formMessage, setFormMessage]  = useState({});
+
+    const handleForm = (e) => {
+        const updateMessage = {
+            ...formMessage,
+            [e.target.name]: e.target.value
+        }
+        setFormMessage(updateMessage);
+    }
+
+    const sendForm = () => {
+        console.log(formMessage)
+    }
+
     return(
         <>
             <Header />
             <Main>
-                <Container>
+                <Container direction='column'>
                     <HeadingH1 text='Contacto'/>
-                    <Paragraph text='Lorem ipsum'/>
+                    <Paragraph text='Si quiere contactar, puede hacerlo a través de este formulario.'/>
                 </Container>
-                <Section>
+                <Section direction='column' align='center'>
                     <Form>
                         <label htmlFor="name">Nombre</label>
-                        <input type="text" name='name'/>
+                        <input  type="text"
+                                name='name'
+                                onBlur={(e) => handleForm(e)}
+                        />
                         <label htmlFor="asunto">Asunto</label>
-                        <input type="text" name='asunto'/>
-                        <label htmlFor="mensaje"></label>
-                        <textarea name="mensaje" cols="30" rows="10"></textarea>
-                        <input type="submit" value="Enviar" name='send'/>
+                        <input  type="text" 
+                                name='asunto'
+                                onBlur={(e) => handleForm(e)}
+                        />
+                        <label htmlFor="mensaje">Mensaje</label>
+                        <textarea   name="mensaje" 
+                                    onBlur={(e) => handleForm(e)}
+                        />
+                        <span onClick={() => sendForm()}>Enviar</span>
                     </Form>
                 </Section>
             </Main>
